5795882ada334092bad0943160521a0fffd12213
Gertkhk <94615923+Gertkhk@users.noreply.github.com>
2021-11-23 15:01:51 +0200

    New and improved code.

--- Komavea parandus kood.py
+++ Komavea parandus kood.py
@@ -1,9 +1,24 @@
+failinimi = input("Mis faili tahate parandada? ")
+uusnimi = input("Mis tahate uue faili nimeks? ")
+
 def paranda_komavead(lause):
     lause = lause.replace(" sest ", ", sest ")
     lause = lause.replace(" et ", ", et ")
     lause = lause.replace(" aga ", ", aga ")
     lause = lause.replace(" kuid ", ", kuid ")
-    print(lause)
+    return lause
 
-paranda_komavead("Ei sest et ei aga ei kuid ei")
-paranda_komavead("Jah et jah aga jah kuid jah sest jah")
+fail = open(failinimi)
+uusfail = open(uusnimi, "w", encoding="UTF-8")
+for a in fail:
+    uus_lause = paranda_komavead(a)
+    uusfail.write(uus_lause)
+    
+fail.close()    
+uusfail.close()
+
+
+
+
+y = paranda_komavead("Ei sest et ei aga ei kuid ei")
+x = paranda_komavead("Jah et jah aga jah kuid jah sest jah")
